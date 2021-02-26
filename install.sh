daf220145f0be58bb3f0b1fdd01aebca4a2d49a1
geonho <16secondlife@gmail.com>
2021-02-26 17:40:20 +0900

    rename deploy directory to manifests

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 set -e
 
-bash deploy/webhook-create-signed-cert.sh
-cat deploy/validating-webhook.yaml | bash deploy/webhook-patch-ca-bundle.sh > deploy/validating-webhook-ca-bundle.yaml
+bash manifests/webhook-create-signed-cert.sh
+cat manifests/validating-webhook.yaml | bash manifests/webhook-patch-ca-bundle.sh > manifests/validating-webhook-ca-bundle.yaml
 
 kubectl apply -f config/crd/tmax.io_signerpolicies.yaml
-kubectl apply -f deploy/role/account.yaml
-kubectl apply -f deploy/role/role.yaml
-kubectl apply -f deploy/role/role-binding.yaml
+kubectl apply -f manifests/role/account.yaml
+kubectl apply -f manifests/role/role.yaml
+kubectl apply -f manifests/role/role-binding.yaml
 
-kubectl apply -f deploy/docker-daemon.yaml
-kubectl apply -f deploy/whitelist-configmap.yaml
-kubectl apply -f deploy/deployment.yaml
-kubectl apply -f deploy/service.yaml
-kubectl apply -f deploy/validating-webhook-ca-bundle.yaml
+kubectl apply -f manifests/docker-daemon.yaml
+kubectl apply -f manifests/whitelist-configmap.yaml
+kubectl apply -f manifests/deployment.yaml
+kubectl apply -f manifests/service.yaml
+kubectl apply -f manifests/validating-webhook-ca-bundle.yaml
 
 echo "Deloying image-validation-webhook completed"
 
